44f5864eeeac5ba2e570d82c705f729ecabe9aba
ElJeringas <santiagojss32@gmail.com>
2023-09-27 17:38:29 -0500

    Implementing NavBar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import NavBar from './components/NavBar/NavBar'
+import Header from './container/Header/Header'
+import AboutUs from './container/AboutUs/AboutUs'
+import SpecialMenu from './container/Menu/SpecialMenu'
+import Chef from './container/Chef/Chef'
+import Intro from './container/Intro/Intro'
+import Laurels from './container/Laurels/Laurels'
+import Gallery from './container/Gallery/Gallery'
+import FindUs from './container/FindUs/FindUs'
+import Footer from './container/Footer/Footer'
+import './App.css'
 const App = () => {
   return (
     <div>
+      <NavBar/>
+      <Header/>
+      <AboutUs/>
+      <SpecialMenu/>
+      <Chef/>
+      <Intro/>
+      <Laurels/>
+      <Gallery/>
+      <FindUs/>
+      <Footer/>
     </div>
   )
 }
--- src/container/Menu/Manu.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Manu = () => {
-  return (
-    <div>Manu</div>
-  )
-}
-
-export default Manu
